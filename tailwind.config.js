78e97eb8bd3ca474b12bbd1919a4fb1b8cf033eb
PolYarik <polyarik@pm.me>
2023-04-21 05:57:32 +0700

    ✨ provide fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        "dark-red": "var(--dark-red)",
-        "standart-red": "var(--standart-red)",
-        "red": "var(--red)",
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+	colors: {
+		"dark-red": "var(--dark-red)",
+		"standart-red": "var(--standart-red)",
+		red: "var(--red)",
 
-        "sec-black": "var(--sec-black)",
-        "sec-purple": "var(--sec-purple)",
-        "sec-white": "var(--sec-white)",
+		"sec-black": "var(--sec-black)",
+		"sec-purple": "var(--sec-purple)",
+		"sec-white": "var(--sec-white)",
 
-        /*where is gray-1 ???*/
-        "gray-2": "var(--gray-2)",
-        "gray-3": "var(--gray-3)",
-        "gray-4": "var(--gray-4)",
+		/*where is gray-1 ???*/
+		"gray-2": "var(--gray-2)",
+		"gray-3": "var(--gray-3)",
+		"gray-4": "var(--gray-4)",
 
-        "black": "var(--black)",
-        "black-2": "var(--black-2)",
-        "black-3": "var(--black-3)",  
-      },
-    },
-  },
-  plugins: [],
-}
+		black: "var(--black)",
+		"black-2": "var(--black-2)",
+		"black-3": "var(--black-3)",
+	},
+	fontFamily: {
+		roboto: ["Roboto", ...defaultTheme.fontFamily.sans],
+		kiona: ["Kiona"],
+	},
+};
+export const plugins = [];
